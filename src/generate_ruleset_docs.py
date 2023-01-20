ab0a4bbc6916407e38f10481ada1b4040e0e3756
Jamie Read <hey@jread.dev>
2023-01-20 21:51:11 +0000

    Try splitting rules into categories

--- src/generate_ruleset_docs.py
+++ src/generate_ruleset_docs.py
@@ -3,19 +3,18 @@ This script is designed to easily generate the documentation from the checks as 
 """
 from check_providers import check_providers
 
-checks = []
 for check_provider in check_providers:
-    for check in check_provider.checks():
-        checks.append(check)
+    checks = check_provider.checks()
+    checks.sort(key=lambda s: s.id)
 
-checks.sort(key=lambda s: s.id)
-
-print("| ID | Severity | Enabled for Project Types | Description | Advice |")
-print("|----|----------|---------------------------|-------------|--------|")
-for check in checks:
-    advice = check.advice.replace("\n", "</br>")
-    severity = check.severity.name.title()
-    print(
-        f"| {check.id} | {severity} | {', '.join(check.project_types)} |"
-        f" {check.reason} | {advice} |"
-    )
+    print(f"### {check_provider.name()}")
+    print("| ID | Severity | Enabled for Project Types | Description | Advice |")
+    print("|----|----------|---------------------------|-------------|--------|")
+    for check in checks:
+        advice = check.advice.replace("\n", "</br>")
+        severity = check.severity.name.title()
+        print(
+            f"| {check.id} | {severity} | {', '.join(check.project_types)} |"
+            f" {check.reason} | {advice} |"
+        )
+    print()
