19d3c47e25f362ce99f49720ace9157832165ad2
Jamie Read <hey@jread.dev>
2023-02-13 19:36:01 +0000

    Add rough JSON support

--- src/japr/japr.py
+++ src/japr/japr.py
@@ -1,6 +1,7 @@
 import argparse
 from japr.check_providers import check_providers
 from japr.check import Result, Severity
+import json
 import os
 import time
 import yaml
@@ -9,7 +10,7 @@ import yaml
 PROJECT_TYPES = ["open-source", "inner-source", "team", "personal"]
 
 
-def _check_directory(directory, project_type, is_summary=False, is_profile=False, fix=False):
+def _check_directory(directory, project_type, is_summary=False, is_profile=False, fix=False, is_json=False):
     directory = os.path.abspath(directory)
     if not os.path.isdir(directory):
         print(f"'{directory}' is not a valid directory so cannot be checked")
@@ -68,69 +69,11 @@ def _check_directory(directory, project_type, is_summary=False, is_profile=False
             start = time.time()
 
     # TODO Group issues by ID for multiple files
-    if is_profile:
+    if is_profile and not is_json:
         print(
             "Profile mode is enabled. Showing all checks performed, not just failed"
             " ones"
         )
-
-    for result, check, profile_time in issues:
-        if (
-            result.result == Result.FAILED and result.id not in ignored_checks
-        ) or is_profile:
-            # Build up components then display for code clarity
-            if result.result == Result.FAILED:
-                emoji_block = "\N{cross mark}"
-            elif result.result == Result.PASSED:
-                emoji_block = "\N{white heavy check mark}"
-            elif result.result == Result.PRE_REQUISITE_CHECK_FAILED:
-                emoji_block = "\N{white question mark ornament}"
-            else:
-                emoji_block = "\N{heavy minus sign}"
-
-            if check.severity == Severity.HIGH:
-                severity_color = "\033[1;31m"
-            elif check.severity == Severity.MEDIUM:
-                severity_color = "\033[1;33m"
-            else:
-                severity_color = "\033[37m"
-
-            profile_block = (str(round(profile_time * 1000)) + "ms").ljust(8)
-
-            if result.file_path is not None:
-                file_block = f"[{result.file_path}] "
-            else:
-                file_block = ""
-
-            if result.fix is not None:
-                fix_block = f" - A fix is available \N{wrench}"
-            else:
-                fix_block = ""
-
-            if is_summary:
-                print(
-                    f"{severity_color}{check.severity.name.ljust(6)}\033[0;0m -"
-                    f" \033[1m{check.id}\033[0;0m {file_block}{check.reason}"
-                    f"{fix_block}"
-                )
-            elif is_profile:
-                print(
-                    f"{emoji_block} {severity_color}{check.severity.name.ljust(6)}\033[0;0m"
-                    f" - {profile_block} - \033[1m{check.id}\033[0;0m"
-                    f" {file_block}{check.reason}{fix_block}"
-                )
-            else:
-                print(
-                    f"{severity_color}{check.severity.name.ljust(6)}\033[0;0m -"
-                    f" \033[1m{check.id}\033[0;0m {file_block}{check.reason}"
-                    f"{fix_block}"
-                )
-                print()
-                print(check.advice)
-                print()
-                print("-" * 10)
-
-    print()
 
     sev_bad_checks = sum(
         check.severity.value
@@ -140,12 +83,6 @@ def _check_directory(directory, project_type, is_summary=False, is_profile=False
     )
     sev_all_checks = sum(check.severity.value for (_, check, _) in issues)
     score = int(5 - sev_bad_checks / sev_all_checks * 5)
-    print(
-        "\033[1mProject score: "
-        + "\N{glowing star}" * score
-        + "\N{heavy minus sign}" * (5 - score)
-        + "\033[0;0m"
-    )
 
     passed = len(
         [
@@ -172,18 +109,95 @@ def _check_directory(directory, project_type, is_summary=False, is_profile=False
     suppressed = len(
         [result for (result, _, _) in issues if result.id in ignored_checks]
     )
-    print(
-        f"\033[1m\033[1;32mPassed: {passed}\033[0;0m, \033[1m\033[1;31mFailed:"
-        f" {failed}\033[0;0m, \033[1m\033[1;37mCannot Run Yet: {cannot_run}, Suppressed"
-        f" {suppressed}\033[0;0m"
-    )
 
-    if score == 5:
+    if is_json:
+        out = {
+                "results": [{"id": result.id, "result": str(result.result), "filePath": result.file_path, "fixAvailable": result.fix is not None, "severity": str(check.severity), "reason": check.reason, "advice": check.advice} for (result, check, _) in issues],
+                "score": score,
+                "passed": passed,
+                "failed": failed,
+                "cannot_run": cannot_run,
+                "suppressed": suppressed
+                }
+        print(json.dumps(out, indent=2))
+    else:
+        for result, check, profile_time in issues:
+            if (
+                result.result == Result.FAILED and result.id not in ignored_checks
+            ) or is_profile:
+                # Build up components then display for code clarity
+                if result.result == Result.FAILED:
+                    emoji_block = "\N{cross mark}"
+                elif result.result == Result.PASSED:
+                    emoji_block = "\N{white heavy check mark}"
+                elif result.result == Result.PRE_REQUISITE_CHECK_FAILED:
+                    emoji_block = "\N{white question mark ornament}"
+                else:
+                    emoji_block = "\N{heavy minus sign}"
+
+                if check.severity == Severity.HIGH:
+                    severity_color = "\033[1;31m"
+                elif check.severity == Severity.MEDIUM:
+                    severity_color = "\033[1;33m"
+                else:
+                    severity_color = "\033[37m"
+
+                profile_block = (str(round(profile_time * 1000)) + "ms").ljust(8)
+
+                if result.file_path is not None:
+                    file_block = f"[{result.file_path}] "
+                else:
+                    file_block = ""
+
+                if result.fix is not None:
+                    fix_block = f" - A fix is available \N{wrench}"
+                else:
+                    fix_block = ""
+
+                if is_summary:
+                    print(
+                        f"{severity_color}{check.severity.name.ljust(6)}\033[0;0m -"
+                        f" \033[1m{check.id}\033[0;0m {file_block}{check.reason}"
+                        f"{fix_block}"
+                    )
+                elif is_profile:
+                    print(
+                        f"{emoji_block} {severity_color}{check.severity.name.ljust(6)}\033[0;0m"
+                        f" - {profile_block} - \033[1m{check.id}\033[0;0m"
+                        f" {file_block}{check.reason}{fix_block}"
+                    )
+                else:
+                    print(
+                        f"{severity_color}{check.severity.name.ljust(6)}\033[0;0m -"
+                        f" \033[1m{check.id}\033[0;0m {file_block}{check.reason}"
+                        f"{fix_block}"
+                    )
+                    print()
+                    print(check.advice)
+                    print()
+                    print("-" * 10)
+
         print()
-        print("\033[1mCongratulations on a fantastic score \U0001F389\033[0;0m")
+        print(
+            "\033[1mProject score: "
+            + "\N{glowing star}" * score
+            + "\N{heavy minus sign}" * (5 - score)
+            + "\033[0;0m"
+        )
 
-    print()
+        print(
+            f"\033[1m\033[1;32mPassed: {passed}\033[0;0m, \033[1m\033[1;31mFailed:"
+            f" {failed}\033[0;0m, \033[1m\033[1;37mCannot Run Yet: {cannot_run}, Suppressed"
+            f" {suppressed}\033[0;0m"
+        )
 
+        if score == 5:
+            print()
+            print("\033[1mCongratulations on a fantastic score \U0001F389\033[0;0m")
+
+        print()
+
+    # TODO handle in JSON
     if fix:
         for result, check, profile_time in issues:
             if result.result == Result.FAILED and result.fix is not None:
@@ -222,9 +236,12 @@ def cli(args=None):
     group.add_argument(
         "--fix", help="experimentally try to fix issues found", action="store_true"
     )
+    group.add_argument(
+        "--json", help="output results as JSON", action="store_true"
+    )
 
     args = parser.parse_args(args)
-    if _check_directory(args.directory, args.project_type, args.summary, args.profile, args.fix):
+    if _check_directory(args.directory, args.project_type, args.summary, args.profile, args.fix, args.json):
         quit(0)
     else:
         quit(1)
